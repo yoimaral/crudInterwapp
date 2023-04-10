4db1357e671f81b575df999cf3efa9862e391c8f
yoimaral <yalozano9@misena.edu.co>
2023-04-10 18:13:15 -0500

    Actualizacion de datos

--- resources/views/car/edit.blade.php
+++ resources/views/car/edit.blade.php
@@ -9,7 +9,7 @@
                 <div class="card-body">
 
 
-                    <form method="POST" action="{{ route('car.store') }}">
+                    <form method="POST" action="{{ route('car.update') }}">
                         @csrf
 
                        <div class="box box-info padding-1">
